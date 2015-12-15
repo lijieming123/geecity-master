c43973903946f174aff996416968d89b6519f85a
lijieming123 <834737996@qq.com>
2015-12-15 09:55:35 +0800

    项目提交

--- app/src/main/java/net/bluemap/geecitypoperty/root/MainActivity.java
+++ app/src/main/java/net/bluemap/geecitypoperty/root/MainActivity.java
@@ -2,31 +2,26 @@ package net.bluemap.geecitypoperty.root;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.umeng.message.ALIAS_TYPE;
-import com.umeng.message.PushAgent;
-import com.umeng.message.UmengRegistrar;
-
+import net.bluemap.geecitypoperty.ExampleUtil;
 import net.bluemap.geecitypoperty.R;
 import net.bluemap.geecitypoperty.common.ShareUtil;
 import net.bluemap.geecitypoperty.common.model.LoginInfo;
 import net.bluemap.geecitypoperty.device.DeviceActivity;
 import net.bluemap.geecitypoperty.notice.NoticeListActivity;
-import net.bluemap.geecitypoperty.order.OrderAcceptActivity;
-import net.bluemap.geecitypoperty.order.OrderCloseActivity;
-import net.bluemap.geecitypoperty.order.OrderDetailActivity;
-import net.bluemap.geecitypoperty.order.OrderListActivity;
 import net.bluemap.geecitypoperty.receive.ReceiveListActivity;
 import net.bluemap.geecitypoperty.room.RoomSelectActivity;
 import net.bluemap.geecitypoperty.root.network.GetWorkbenchWSI;
@@ -34,16 +29,15 @@ import net.bluemap.geecitypoperty.task.TaskListActivity;
 import net.tsz.afinal.FinalActivity;
 import net.tsz.afinal.annotation.view.ViewInject;
 
+import java.util.Set;
+
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.api.TagAliasCallback;
 import hz.toollib.interfaces.WebAPIListener;
 
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
-    @ViewInject(id = R.id.btn_receive)
-    Button mBtnReceive;
-    @ViewInject(id = R.id.btn_rob_close)
-    Button mBtnClose;
-    @ViewInject(id = R.id.btn_detail)
-    Button mBtnDetail;
+
     @ViewInject(id = R.id.am_srl_refresh) SwipeRefreshLayout srlRefresh;
 
     @ViewInject(id = R.id.am_ll_notice, click = "onClick")
@@ -69,71 +63,48 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     TextView tvTask;
     @ViewInject(id = R.id.am_tv_username)
     TextView tvUsername;
-    @ViewInject(id=R.id.btn_rob_bill)
-     Button mBtnRobBill;
+
     LoginInfo li;
-    PushAgent mPushAgent;
+
     //接口
     GetWorkbenchWSI getWorkbenchWSI;
-
+    public static boolean isForeground = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FinalActivity.initInjectedView(this);
-        initView();
-        mPushAgent = PushAgent.getInstance(getApplicationContext());
-        PushAgent.getInstance(getApplicationContext()).onAppStart();
-        String device_token = UmengRegistrar.getRegistrationId(getApplicationContext());
-        Log.d("设备token",device_token);
-        mBtnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentClose = new Intent(MainActivity.this, OrderCloseActivity.class);
-                startActivity(intentClose);
-            }
-        });
-        mBtnDetail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentDetail=new Intent(MainActivity.this, OrderDetailActivity.class);
-                startActivity(intentDetail);
-            }
-        });
-        mBtnReceive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intentReceive=new Intent(MainActivity.this, OrderAcceptActivity.class);
-                startActivity(intentReceive);
-            }
-        });
-        mBtnRobBill.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //打开抢单列表界面
-                Intent intent=new Intent(MainActivity.this, OrderListActivity.class);
-                startActivity(intent);
-            }
-        });
-         //应用程序启动统计
-        //参考集成文档的1.5.1.2
-        //http://dev.umeng.com/push/android/integration#1_5_1
-        mPushAgent.onAppStart();
-        PushAgent.sendSoTimeout(this, 600);	//设置护保进程间隔时间
-        mPushAgent.enable();
-    }
 
+        initView();
+        //推送初始化
+        init();
+        //绑定广播接收
+        registerMessageReceiver();
+        //设置设备别名
+//        String deviceId=JPushInterface.getRegistrationID(getApplication());
+
+
+
+
+
+
+
+    }
+    private void init() {
+        JPushInterface.init(getApplicationContext());
+    }
     @Override
     protected void onResume() {
+        isForeground = true;
         super.onResume();
         loadData();
-        Log.d("用户id",li.getId());
-        try {
-            mPushAgent.addAlias(li.getUserName(), ALIAS_TYPE.BAIDU);
-            mPushAgent.addExclusiveAlias(li.getUserName(), ALIAS_TYPE.SINA_WEIBO);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        JPushInterface.setAlias(getApplicationContext(), li.getUserName(), new TagAliasCallback() {
+            @Override
+            public void gotResult(int i, String s, Set<String> set) {
+                Log.d("Tag", "" + i + s);
+            }
+        });
+
 
     }
 
@@ -236,6 +207,54 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 startActivity(intent);
                 finish();
             }
-        }).setNegativeButton("否",null).create().show();
+        }).setNegativeButton("否", null).create().show();
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        isForeground = false;
+        super.onPause();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mMessageReceiver);
+        super.onDestroy();
+    }
+
+
+    //for receive customer msg from jpush server
+    private MessageReceiver mMessageReceiver;
+    public static final String MESSAGE_RECEIVED_ACTION = "com.example.jpushdemo.MESSAGE_RECEIVED_ACTION";
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_EXTRAS = "extras";
+
+    public void registerMessageReceiver() {
+        mMessageReceiver = new MessageReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
+        filter.addAction(MESSAGE_RECEIVED_ACTION);
+        registerReceiver(mMessageReceiver, filter);
+    }
+
+    public class MessageReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (MESSAGE_RECEIVED_ACTION.equals(intent.getAction())) {
+                String messge = intent.getStringExtra(KEY_MESSAGE);
+                String extras = intent.getStringExtra(KEY_EXTRAS);
+                StringBuilder showMsg = new StringBuilder();
+                showMsg.append(KEY_MESSAGE + " : " + messge + "\n");
+                if (!ExampleUtil.isEmpty(extras)) {
+                    showMsg.append(KEY_EXTRAS + " : " + extras + "\n");
+                }
+
+            }
+        }
     }
 }
